c39025f61a74bc8b7ced45a112a64c6c2b3c5eb7
issachar-j <yisakoreyob9@gmail.com>
2024-08-23 09:10:06 +0300

    moke index.js

--- src/pages/LandingPages/ContactUs/index.js
+++ src/pages/LandingPages/ContactUs/index.js
@@ -146,30 +146,11 @@ function ContactUs() {
       width: 200,
       editable: false,
       filter: "agTextColumnFilter",
-      cellrenderer: (params) =>  {
-        let formattedDate = "Loading..."
-        db.collection("user").doc(params.value).get().then((doc) => {
-          const createTime = doc.createTime.toDate();
-          const monthNames = [
-            "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-          ];
-            const dayNames = [
-                "Sun", "Mon", "Tue", "Wed", 
-                "Thu", "Fri", "Sat"
-            ];
-          const dayOfWeek = dayNames[createTime.getDay()];
-          const month = monthNames[createTime.getMonth()];
-          const day = ("0" + createTime.getDate()).slice(-2);
-          const year = createTime.getFullYear();
-          formattedDate = `${dayOfWeek}, ${month} ${day}, ${year}`;
-        }
-       return (
-        <p>
-           ${formattedDate}
-        </p>                                                        
-      )
-      }
+      cellRenderer: (param) => {
+    const cellElement = document.createElement('span');
+    cellElement.textContent = `timestamp of ${param.value}`;
+    return cellElement;
+  }
     }
     
   ];
